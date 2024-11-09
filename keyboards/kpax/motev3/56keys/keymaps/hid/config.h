0b4e5e4dc5066ec0024ac5b157439e28dbf2a4b9
kpaxart <07genics@gmail.com>
2024-11-09 22:38:24 +0800

    v3编译成功

--- keyboards/kpax/motev3/56keys/keymaps/hid/config.h
+++ keyboards/kpax/motev3/56keys/keymaps/hid/config.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID \
-    { 0x2B, 0x3C, 0x7E, 0x52, 0x60, 0x3E, 0xFE, 0x52 }
+    { 0x37, 0xD1, 0xA9, 0xDF, 0x86, 0xA2, 0xC5, 0xCA }
 
 /* VIAL secure unlock keystroke - currently both inner thumb keys (typ. SPACE/ENTER) */
 #define VIAL_UNLOCK_COMBO_ROWS \
@@ -15,6 +15,5 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
 #define VIAL_COMBO_ENTRIES 20
 #define DYNAMIC_KEYMAP_MACRO_COUNT 50
-
 
 #define SPLIT_TRANSACTION_IDS_USER RPC_ID_USER_HID_SYNC, RPC_ID_USER_CAPS_WORD_SYNC, RPC_ID_USER_LAYER_SYNC
