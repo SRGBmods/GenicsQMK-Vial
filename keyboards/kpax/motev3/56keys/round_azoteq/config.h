81fe268ad417478cbe87e0d0e27d43b722e6f5ca
kpaxart <07genics@gmail.com>
2024-11-15 22:23:40 +0800

    加入azoteq配置

---
+++ keyboards/kpax/motev3/56keys/round_azoteq/config.h
@@ -0,0 +1,15 @@
+// Copyright 2023 zzeneg (@zzeneg)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// azoteq触控板配置选项
+#define AZOTEQ_IQS5XX_TPS43
+#define AZOTEQ_IQS5XX_ROTATION_90
+#define POINTING_DEVICE_INVERT_Y
+// 设备除数，越小越快
+
+// 点设备的任务更新时间
+#define POINTING_DEVICE_TASK_THROTTLE_MS 15
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
